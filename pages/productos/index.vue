3bb03eace0a6bd35ff8ad72adaf1584b6f92c55a
JohnSnow18 <jhonjajoy1803@gmail.com>
2019-04-09 20:20:40 -0500

    Paginacion_boton_Editar_Eliminar

--- pages/productos/index.vue
+++ pages/productos/index.vue
@@ -4,7 +4,26 @@
 
     <div style="row mt-2">
       <div style="col-sm-12">
-        <b-table striped hover :fields="fields" :items="items"></b-table>
+        <b-table id= "productos" striped hover :fields="fields" :items="items" :current-page="currentPage" :per-page="perPage">
+
+          <template slot="acciones">
+            <b-button variant="success">
+              Editar
+            </b-button>
+
+              <b-button variant="danger">
+              Eliminar
+            </b-button>
+           
+          </template>
+        </b-table>
+
+      <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="productos"
+    ></b-pagination>
       </div>
     </div>
     <!-- :items="items" -->
@@ -18,19 +37,29 @@
 
 <script>
 import firebase, {db} from "../../services/firebase";
+/* import { async } from 'q'; */
 
 export default {
   asyncData() {
     // se ejecuta antes de cargar la pagina
-    return db.collection('productos').get().then(res =>{
+    let perPage = 2
+    return db.collection('productos')/* .orderBy('nombre') *//* .limit(perPage) */.get().then(async res =>{
       let items = []
      console.log(res)
      res.forEach(value =>{
        items.push(value.data())
      })
-     //console.log(items)
+
+/* let resTotal = await db.collection('productos').get()
+     let ultimoItem = items[items.length -1] */
+     console.log(items)
       return{
-        items
+        items,
+     /*    ultimoItem, */
+        currentPage: 1,
+        /* rows: resTotal.docs.length, */
+        rows:res.docs.length,
+        perPage
       }
     })
   },
@@ -41,10 +70,22 @@ export default {
     return {
       fields: [
         "imagen",
-        "nombre",
-        "precio",
-        "cantidad",
-        "acciones"
+        {
+          key: "nombre",
+          sortable: true
+        },
+        {
+          key: "precio",
+          sortable: true,
+        },
+        {
+          key: "cantidad",
+          sortable: true,
+        },
+        {
+          key: "acciones",
+          sortable: false
+        }
         /*items:
            { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
           { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
